cf128b89ab3aa6912d6832878564ae38912a8e27
Bruna Santos <brunahta2025@hotmail.com>
2022-09-12 18:04:25 -0300

    first

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,24 @@
+export default{
+    colors:{
+        primary: '#FD98CF',
+
+        secundary: '#7D2556',
+        secundary_light: 'rgba(125, 37, 86, 0.3)',
+
+        success: '#257D4C',
+        success_light: 'rgba(37, 125, 76, 0.3)',
+
+        attentation: '#7D252A',
+        attentation_light: 'rgb(125, 37, 42, 0.3)',
+
+        shape: '#ffffff',
+        title: '#252A7D',
+        text: '#969cb2',
+        background: '#f0f2f5'
+    },
+    fonts:{
+       regular:  'Poppins_400Regular',
+       medium:  'Poppins_500Medium',
+       bold: ' Poppins_700Bold'
+    }
+}
