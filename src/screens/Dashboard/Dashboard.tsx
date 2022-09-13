baa5247833ea1503988bfd0622586b114f4cad75
Bruna Santos <brunahta2025@hotmail.com>
2022-09-13 18:20:36 -0300

    dashboard

--- src/screens/Dashboard/Dashboard.tsx
+++ src/screens/Dashboard/Dashboard.tsx
@@ -1,16 +1,49 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { Container } from "./styles";
+import { Entypo } from '@expo/vector-icons'; 
+import { 
+    Container,
+    Header,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    UserWrapper,
+    HighlightCards,
+    Icon
+
+ } from "./styles";
+import { HighlightCard } from "../../components/HighlightCard";
+
 
 export function Dashboard(){
     return(
-        <Container>
-            <Text>
-                Dashboard ola
-            </Text>
 
-        </Container>
       
+        <Container>
+
+            <Header>
+                <UserWrapper>
+                <UserInfo>
+                    <Photo source={{uri: 'https://picsum.photos/200' }}/>
+                    <User>
+                        <UserGreeting>Olá, </UserGreeting>
+                        <UserName>Name</UserName>
+                    </User>
+                </UserInfo>
+                  <Icon name="log-out"/>
+                </UserWrapper>
+            </Header>
+      
+            <HighlightCards
+            >
+                <HighlightCard />
+                <HighlightCard />
+                <HighlightCard />
+                
+            </HighlightCards>
+        </Container>
+
 
     )
 }
