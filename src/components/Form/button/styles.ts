0dc268fb7f0ca590b8565a154f5e63d8159774d7
Bruna Santos <brunahta2025@hotmail.com>
2022-09-15 17:47:04 -0300

    register button

---
+++ src/components/Form/button/styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components/native";
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+
+
+
+export const Container = styled.TouchableOpacity`
+width: 100%;
+padding: 18px;
+
+
+background-color: ${({theme}) => theme.colors.secundary};
+border-radius: 5px;
+align-items: center;
+
+`;
+export const Title = styled.Text`
+font-size:   ${RFValue(15)}px;
+font-family: ${({theme}) => theme.fonts.medium}; 
+color:  ${({theme}) => theme.colors.shape}; 
+
+`
