bb12ef75482520ec20c045cfc603ce109104ed04
Bruna Santos <brunahta2025@hotmail.com>
2022-09-16 20:10:57 -0300

    Category Name&Icons

---
+++ src/utils/categorias.ts
@@ -0,0 +1,8 @@
+export const categorias = [
+    {key: 'purchase', name: 'Compras', icon: 'shopping-bag', color: '#5636d3'},
+    {key: 'food', name: 'Alimentação', icon: 'coffee', color: '#ff872c'}, //import { MaterialIcons } from '@expo/vector-icons'; 
+    {key: 'salary', name: 'Salário', icon: 'dollar-sign', color: '#12a454'},
+    {key: 'car', name: 'Carro', icon: 'crosshair', color: '#e83f5b'},
+    {key: 'studies', name: 'Estudos', icon: 'book', color: '#26195c'},
+    {key: 'other', name: 'Outro', icon: 'plus', color: '#9c001a'}, //import { AntDesign } from '@expo/vector-icons';
+]
