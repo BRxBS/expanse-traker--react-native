f8516a7a4ef82caf813ff4d838a059b65476608e
Bruna Santos <brunahta2025@hotmail.com>
2022-09-19 16:25:59 -0300

    cleaning

--- src/screens/CategorySelect/index.tsx
+++ src/screens/CategorySelect/index.tsx
@@ -38,6 +38,8 @@ export function CategorySelect({
     function handleCategorySelect(category: Category) {
         setCategory(category);
       }
+
+
     return(
         <>
         <Container >
