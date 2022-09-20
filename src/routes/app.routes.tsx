fec6e1d4e5f02aa4226e5a47f1abadce67851e02
Bruna Santos <brunahta2025@hotmail.com>
2022-09-20 20:58:36 -0300

    routes react-navigation

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import {Platform} from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { useTheme } from "styled-components";
+import {MaterialIcons} from '@expo/vector-icons'
+
+import { Dashboard } from "../screens/Dashboard/Dashboard";
+import { Register } from "../screens/Register";
+
+const Tab = createBottomTabNavigator();
+
+
+export function AppRoutes(){
+    
+    const theme = useTheme()
+    return(
+        <Tab.Navigator
+        screenOptions={{
+            tabBarActiveTintColor: theme.colors.secundary,
+            tabBarInactiveTintColor: theme.colors.text,
+            tabBarLabelPosition: 'beside-icon',
+            tabBarStyle: {
+                paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+                height: 60
+            }
+        }}
+        >
+            <Tab.Screen
+            name="Listagem"
+            component={Dashboard}
+            options={{
+                tabBarIcon: (({size, color}) => (
+                    <MaterialIcons
+                    name="format-list-bulleted"
+                    size={size}
+                    color={color}
+                    />
+                )),
+                headerShown: false
+            }}
+            />
+           <Tab.Screen
+            name="Cadastrar"
+            component={Register}
+            options={{
+                tabBarIcon: (({size, color}) => (
+                    <MaterialIcons
+                    name="attach-money"
+                    size={size}
+                    color={color}
+                    />
+                )),
+                headerShown: false ,
+            }}
+        
+            />
+            <Tab.Screen
+            name="Resumo"
+            component={Register}
+            options={{
+                tabBarIcon: (({size, color}) => (
+                    <MaterialIcons
+                    name="pie-chart"
+                    size={size}
+                    color={color}
+                    />
+                )),
+                headerShown: false
+            }}
+            
+            />
+        </Tab.Navigator>
+    )
+}
