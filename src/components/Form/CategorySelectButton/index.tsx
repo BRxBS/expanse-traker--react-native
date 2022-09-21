003038785fd464b28a26d7289e055f036fa76dae
Bruna Santos <brunahta2025@hotmail.com>
2022-09-21 20:36:46 -0300

    cleaning

--- src/components/Form/CategorySelectButton/index.tsx
+++ src/components/Form/CategorySelectButton/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { TouchableOpacityProps } from "react-native";
 import { Category, Container, Icon } from "./styles";
-import { RectButton } from 'react-native-gesture-handler';
+
 
 interface Props extends TouchableOpacityProps {
   title: string;
