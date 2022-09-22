bb289d6bb06fc4b03f4ca6fc51f50f6d8772d14f
Bruna Santos <brunahta2025@hotmail.com>
2022-09-22 16:11:11 -0300

    navigation Resumo

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -2,7 +2,9 @@ export declare global {
     namespace ReactNavigation{
         interface RootParamList{
             Listagem: undefined;
-            Register: undefined
+            Register: undefined;
+            Resume:undefined
+            
         }
     }
 }
