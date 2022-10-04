b7039abcfc3f9a1c952f47495315d53a37bfedca
Bruna Santos <brunahta2025@hotmail.com>
2022-10-04 13:31:15 -0300

    auth

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,8 +1,8 @@
-import React, { createContext, ReactNode, useContext, useState } from "react";
+import React, { createContext, ReactNode, useContext, useEffect, useState } from "react";
 import * as AuthSession from 'expo-auth-session';
 import AsyncStorage from "@react-native-async-storage/async-storage";
-// const {CLIENT_ID} = process.env
-// const {REDIRECT_URI} = process.env
+const {CLIENT_ID} = process.env
+const {REDIRECT_URI} = process.env
 
 type AuthResponse = {
     params: {
@@ -10,9 +10,6 @@ type AuthResponse = {
 
     };
     type: string
-}
-type Param ={
-    token: string
 }
 
 interface TransactionsProviderProps{
@@ -28,55 +25,74 @@ type User = {
 interface AuthContextData {
     user: User;
     signInWithGoogle(): Promise<void>;
-    // signOut(): Promise<void>;
-    // userStorageLoading: boolean;
+    signOut(): Promise<void>;
+    userStorageLoading: boolean;
   }
 
 export const AuthContext = createContext({} as AuthContextData);
 
 function AuthProvider({children} : TransactionsProviderProps ){
     const [user, setUser] = useState<User>({} as User);
-
+    const [userStorageLoading, setUserStorageLoading] = useState(true);
+    const userStorageKey = `@finance:transactions`
 
     async function signInWithGoogle() {
-        // try{
-            const CLIENT_ID="83878190207-ur5m6t4ov5r0cl5od83tjgaisqban1pc.apps.googleusercontent.com"
-    const REDIRECT_URI="https://auth.expo.io/@anonymous/finance-f0cf056c-c970-4d3f-8d8d-33f155b37c88"
+        try{
+
         const RESPONSE_TYPE = 'token'
         const SCOPE = encodeURI('profile email')
 
         const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`
        
-        const response = await AuthSession
+        const {type, params} = await AuthSession
         .startAsync({ authUrl }) as AuthResponse
-        console.log('response', response)
 
-        // console.log('type', type)
-        // console.log('params', params)
-
-        
-        // const token = params.access_token
-        // console.log('token', token)
-
+        console.log('type', type)
+        console.log('params', params)
     
-        // const response2 = await fetch(`https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token=${response.params.access_token}`)
+        const response2 = await fetch(`https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token=${params.access_token}`)
             
-        // if (response.type === "success") {
-        //     const userLogged = await response2.json()
-        //     setUser(userLogged);
+        if (type === "success") {
+            const userLogged = await response2.json()
+            setUser(userLogged);
+            await AsyncStorage.setItem(userStorageKey, JSON.stringify(userLogged));
         
-        //       }
-        //     } catch (err) {
-        //         console.log('error signInWithGoogle',err)
-        //       }
+              }
+            } catch (err) {
+                console.log('error signInWithGoogle',err)
+              }
     }
-    
 
+    async function signOut(){
+        setUser({} as User);
+        await AsyncStorage.removeItem(userStorageKey)
+
+    }
+
+    useEffect(() => {
+        async function loadUserStorageData() {
+          const userStoraged = await AsyncStorage.getItem(userStorageKey);
+          if (userStoraged) {
+            const userLogged = JSON.parse(userStoraged) as User;
+            setUser(userLogged);
+          }
+          setUserStorageLoading(false);
+        }
+        loadUserStorageData();
+      }, []);
+    
 
     return(
-        <AuthContext.Provider value={ { user,
-                                        signInWithGoogle }}>
+        <AuthContext.Provider 
+        value={ { 
+       user,
+       signInWithGoogle,
+       signOut,
+       userStorageLoading
+ }}>
+
             {children}
+
         </AuthContext.Provider>
     )
 }
