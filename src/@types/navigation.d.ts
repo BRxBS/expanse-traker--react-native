38adcc859b9f40d61447cbcad28f7bab9eb9cb8d
Bruna Santos <brunahta2025@hotmail.com>
2022-10-11 17:51:52 -0300

    params

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -1,21 +1,25 @@
 export declare global {
     namespace ReactNavigation{
   export   interface RootParamList{
-            Listagem:  {
-                params: object;
-                merge: boolean
-            };
-            Register: undefined;
-            Resume:undefined;
-            EditScreen: {
-                id: string;
-                type: 'positive' | 'negative';
+            Listagem:  {id: string;
                 name: string;
                 amount: string;
-                category: string ;
+}
+            Register: undefined;
+            Resume:undefined;
+            EditScreen: {id: string;
+                        name: string;
+                        amount: string;
+     }
+            // {
+            //     id: string;
+            //     type: 'positive' | 'negative';
+            //     name: string;
+            //     amount: string;
+            //     category: string ;
 
 
-            }
+            // }
             
         }
     }
