4d47f7f4a39ed117899fb9f754ca270218c206c4
Bruna Santos <brunahta2025@hotmail.com>
2022-10-11 17:52:42 -0300

    TextInput

--- src/screens/Edit/styles.ts
+++ src/screens/Edit/styles.ts
@@ -44,3 +44,15 @@ margin-top: 8px;
 margin-bottom: 16px;
 
 `
+export const TextInput = styled.TextInput`
+width: 100%;
+padding: 16px 18px;
+font-family: ${({theme}) => theme.fonts.regular}; 
+font-size:  ${RFValue(14)}px;
+
+color: ${({theme}) => theme.colors.text_dark};
+background-color: ${({theme}) => theme.colors.shape};
+border-radius: 5px;
+margin-bottom: 10px;
+
+`;
